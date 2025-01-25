c3ca05d5ae0eb5c3e95ab3dfd9c8814c2b91a508
Travis Waterman <106386185+watet3953@users.noreply.github.com>
2025-01-25 16:19:15 -0500

    package

---
+++ Assets/Scripts/CardHand.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardHand : MonoBehaviour
+{
+    public GameObject[] cards;
+
+    public void RemoveCard(GameObject card)
+    {
+
+    }
+}
