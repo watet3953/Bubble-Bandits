0a01fa76dbbff39fba2e6ca8f11cbcd55a70b905
Travis Waterman <106386185+watet3953@users.noreply.github.com>
2025-01-26 12:12:53 -0500

    fix camera

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -55,7 +55,7 @@ public class MapManager : MonoBehaviour
 
     public void Update()
     {
-        if ((camera.transform.position - moveTo).magnitude > 1f)
+        if (camera != null && (camera.transform.position - moveTo).magnitude > 1f)
         {
             camera.transform.position = Vector3.Lerp(camera.transform.position, moveTo, 0.1f);
 
