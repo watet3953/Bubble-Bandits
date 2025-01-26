5de26a2c5b427925fac55a977b0dc09635649328
The-Gilded-Dumpster <is21zo@brocku.ca>
2025-01-25 20:59:47 -0500

    Chewy is the Icon
    Chewy M'Gooey is the Game Icon!!!

--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Scroll : MonoBehaviour
 {
-    [SerializeField] private RawImage img;
-    [SerializeField] private float x;
-    [SerializeField] private float y;
+    [SerializeField] private float speed;
+
+    [SerializeField] private Renderer bRenderer;
 
     // Update is called once per frame
     void Update()
     {
-        img.uvRect = new Rect(img.uvRect.position + new Vector2(x, y) * Time.deltaTime, img.uvRect.size);
+        bRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
     }
 }
